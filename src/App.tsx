a042648b649b1dabd838e632f66c21dfe5fcb234
hanheel <hanhee630630@gmail.com>
2025-11-16 15:12:30 +0900

    chore: 폰트 다운로드 및 토큰 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./styles/global.css";
 
 const App = () => {
-  return <h1>Main</h1>;
+  return <div className="p-10 bg-blue-500 text-white text-2xl font-bold">Tailwind 정상 작동!</div>;
 };
 
 export default App;
