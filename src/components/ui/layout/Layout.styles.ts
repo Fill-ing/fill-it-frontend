4163d20ca472abb1d1be79d468aa5f4dcbf7f88b
Hanhee Lee <168459001+hanheel@users.noreply.github.com>
2025-12-09 20:27:20 +0900

    [feature] 모바일 레이아웃 설정 (#10)
    * refactor: domain 컴포넌트 폴더 component 하위로 이동
    
    * feature: 레이아웃 컴포넌트 및 스타일 설정
    
    * feature: 라우터에 Layout 연결 및 RouterProvider 설정
    
    * design: 가운데 정렬 추가
    
    * chore: auto review 개행 수정
    
    * chore: yaml 파싱 오류 해결
    
    * chore: 지원되지 않는 yaml 설정 삭제
    
    * chore: 코드 래빗 기본 설정 삭제

---
+++ src/components/ui/layout/Layout.styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  max-width: ${({ theme }) => theme.layout.width};
+  width: 100%;
+  height: 100dvh;
+  background-color: ${({ theme }) => theme.colors.background};
+  padding: ${({ theme }) => `${theme.layout.paddingY} ${theme.layout.paddingX}`};
+  margin: 0 auto;
+`;
